6eaca2bb205e8d45ba577efb170eee9f9e24afcc
Tanda Hamonangan <tanda.hamonangan@outlook.com>
2017-01-18 23:53:19 -0500

    second update on file and cs file

--- TestSolution/Test1.cs
+++ TestSolution/Test1.cs
@@ -4,6 +4,8 @@ namespace Test.Controllers
 {
     public class AccountController : Controller
     {
-	
+	    int initialValue = 0;
+	    Console.WriteLine("Test updating: {0}", initialValue);
+		Console.ReadKey();
 	}
 }
